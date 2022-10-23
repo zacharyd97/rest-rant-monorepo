386251f98ad5a611ce8b055348b935cdec0d2c67
Zachary Dowell <zdowell97@gmail.com>
2022-10-23 13:32:25 -0400

    sessiom

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 const bcrypt = require("bcrypt");
-const jwt = require("json-web-token");
+
 const { User } = db;
 
 router.post("/", async (req, res) => {
@@ -13,42 +13,29 @@ router.post("/", async (req, res) => {
     !user ||
     !(await bcrypt.compare(req.body.password, user.passwordDigest))
   ) {
-    res.status(404).json({
-      message: `Could not find a user with the provided username and password`,
-    });
-  } else {
-    const result = await jwt.encode(process.env.JWT_SECRET, {
-      id: user.userId,
-    });
-    res.json({ user: user, token: result.value });
-  }
-});
-
-router.get("/profile", async (req, res) => {
-  try {
-    // Split the authorization header into [ "Bearer", "TOKEN" ]:
-    const [authenticationMethod, token] = req.headers.authorization.split(" ");
-
-    // Only handle "Bearer" authorization for now
-    //  (we could add other authorization strategies later):
-    if (authenticationMethod == "Bearer") {
-      // Decode the JWT
-      const result = await jwt.decode(process.env.JWT_SECRET, token);
-
-      // Get the logged in user's id from the payload
-      const { id } = result.value;
-
-      // Find the user object using their id:
-      let user = await User.findOne({
-        where: {
-          userId: id,
-        },
+    res
+      .status(404)
+      .json({
+        message: `Could not find a user with the provided username and password`,
       });
-      res.json(user);
-    }
-  } catch {
-    res.json(null);
+  } else {
+    req.session.userId = user.userId;
+    res.json({ user });
   }
 });
+
+router.get('/profile', async (req, res) => {
+  try {
+      let user = await User.findOne({
+          where: {
+              userId: req.session.userId
+          }
+      })
+      res.json(user)
+  } catch {
+      res.json(null)
+  }
+})
+
 
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
-// Modules and Globals
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const cookieSession = require("cookie-session");
+const defineCurrentUser = require("./middleware/defineCurrentUser");
 
 // Express Settings
-app.use(cors());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.SESSION_SECRET],
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(defineCurrentUser);
 
 // Controllers & Routes
 
@@ -18,8 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/places", require("./controllers/places"));
 app.use("/users", require("./controllers/users"));
 app.use("/authentication", require("./controllers/authentication"));
-// Listen for Connections
 
+// Listen for Connections
 app.listen(process.env.PORT, () => {
   console.log(`Listening on ${process.env.PORT}`);
 });
